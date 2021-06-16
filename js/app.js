110443a743877203e384672a405f760b9067064f
yousoumar <oumaiyr@gmail.com>
2021-06-16 02:26:39 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 "use strict";
 const cards = document.querySelector('#cards');
-console.log(cards)
+const stays = [];
 fetch('js/stays.json')
 .then(response => {
     return response.json();
 })
 .then(data => {
-    console.log(data);
     data.forEach(element => {
+        console.log(element)
+        stays.push(element);
         let card = document.createElement('div');
         card.classList.add('card')
-        card.innerHTML = `<div class = "img"><img src = ${element.photo} /></div>`
         cards.appendChild(card);
+        if(element.superHost){
+            card.innerHTML = `<div class = "img">
+                                <img src = ${element.photo} />
+                              </div>
+                              <div class ="details">
+                                <div class = "superhost"> SUPER HOST</div>
+                                <div class ="type">${element.type}</div>
+                                <div class ="rating"><i class="fas fa-star"></i> ${element.rating}</div>
+                              </div>`
+
+        }else{
+            card.innerHTML = `<div class = "img">
+                                <img src = ${element.photo} />
+                              </div>
+                              <div class ="details">
+                                <div class ="type">${element.type}</div>
+                                <div class ="rating"><i class="fas fa-star"></i> ${element.rating}</div>
+                              </div>`
+        }
     });
 });
+
